25ff0d35952e2a2572573a73ad0c210f4106836f
Joseph Tingsanchali <jtingsan@gmail.com>
2015-11-29 02:11:54 -0600

    Verify immutability of List data structure

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -1,4 +1,5 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutability', () => {
 
@@ -14,6 +15,30 @@ describe('immutability', () => {
 
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
+    });
+
+  });
+
+  describe('a list', () => {
+    
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Pulp Fiction', 'The Matrix');
+      let nextState = addMovie(state, 'Casino');
+
+      expect(nextState).to.equal(List.of(
+        'Pulp Fiction',
+        'The Matrix',
+        'Casino'
+      ));
+      expect(state).to.equal(List.of(
+        'Pulp Fiction',
+        'The Matrix'
+      ));
+
     });
 
   });
